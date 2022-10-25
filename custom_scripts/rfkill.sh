0f9ad11ef30b37487b295fecd669bdb740ceba9d
Kirill Moiseevskikh <cyruran@mail.ru>
2022-10-25 22:50:39 +0300

    dotfiles

---
+++ custom_scripts/rfkill.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+type=$1
+
+output=$(/usr/sbin/rfkill list -no SOFT $type)
+
+if [[ $output == "blocked
+blocked" ]]; then
+    status=blocked
+    color=red
+elif [[ $output == "unblocked
+unblocked" ]]; then
+    status=unblocked
+    color=green
+else
+    status=mixed
+    color=yellow
+fi
+
+if [[ $BLOCK_BUTTON == "1" ]]; then
+    case "$status" in
+        mixed|unblocked)
+            sudo rfkill block $type
+            ;;
+        *)
+            sudo rfkill unblock $type
+            ;;
+    esac
+    BLOCK_BUTTON="" $0 $type
+else
+    echo "$type: <span color=\"$color\">$status</span>"
+fi
